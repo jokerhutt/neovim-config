78b46fd92339dfd89b98f1535b1ce34fc8cda169
Dave <davidglogowski2003@gmail.com>
2025-08-19 21:55:46 +0200

    add system clipboard leader bindings

--- lua/jokerhut/remap.lua
+++ lua/jokerhut/remap.lua
@@ -1,3 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>")
+
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+
+vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("v", "<leader>p", '"+p')
