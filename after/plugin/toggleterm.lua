34d568a8aa239843e66a3cd1ce30bdf061c463b3
Dave <davidglogowski2003@gmail.com>
2025-09-08 00:38:01 +0200

    Add escape key to enter vim normal mode on toggleterm

---
+++ after/plugin/toggleterm.lua
@@ -0,0 +1,7 @@
+-- leave terminal mode with <Esc>
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = "term://*",
+	callback = function()
+		vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { buffer = true })
+	end,
+})
