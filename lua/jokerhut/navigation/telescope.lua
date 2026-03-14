1dae4104b3f8325d2e521df8eb9d39f11e0e59f7
Dave <davidglogowski2003@gmail.com>
2026-03-14 20:42:18 +0100

    add telescope keybindings & set default color to tokyo night

--- lua/jokerhut/navigation/telescope.lua
+++ lua/jokerhut/navigation/telescope.lua
@@ -1,7 +1,8 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Find files in project" })
+vim.keymap.set("n", "<leader>pg", builtin.git_files, { desc = "Find git files" })
+vim.keymap.set("n", "<leader>p/", builtin.current_buffer_fuzzy_find, { desc = "Search in current file" })
 
 vim.keymap.set("n", "<leader>ps", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+end, { desc = "Find string in project" })
