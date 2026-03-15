4b39d2192380e209304efd3e9d882e80fa1d586d
Dave <davidglogowski2003@gmail.com>
2026-03-15 22:27:01 +0100

    open live grep immediately on leader ps

--- lua/jokerhut/navigation/telescope.lua
+++ lua/jokerhut/navigation/telescope.lua
@@ -3,6 +3,4 @@ vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Find files in pr
 vim.keymap.set("n", "<leader>pg", builtin.git_files, { desc = "Find git files" })
 vim.keymap.set("n", "<leader>p/", builtin.current_buffer_fuzzy_find, { desc = "Search in current file" })
 
-vim.keymap.set("n", "<leader>ps", function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end, { desc = "Find string in project" })
+vim.keymap.set("n", "<leader>ps", builtin.live_grep, { desc = "Search in project" })
